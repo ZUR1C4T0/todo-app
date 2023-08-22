5db2ccea0e91f1f90fedf630209984f4f90ceb9b
ZUR1C4T0 <ivanulloque19@gmail.com>
2023-08-22 10:03:28 -0500

    root route

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class App {
+
+    @GetMapping
+    public String getHello(){
+        return "Hello World!";
+    }
+
+}
